f2f9ee6e67cde9a460f1a463390f3fe2fc2b8496
Tomb909 <thomas.barker.38@icloud.com>
2025-12-27 20:58:54 +0000

    Isolate main logic from chip.cpp

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "chip8.h"
+
+chip8 Chip8;
+
+int main(int argc, char **argv) {
+
+    Chip8.initialise();
+
+    if (argc < 2) {
+        printf("Usage: ./chip8 gameFileName\n");
+    }
+
+    Chip8.loadGame(argv[1]);
+    
+    return 0;
+}
